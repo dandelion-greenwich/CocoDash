0affca39b1e1f7c8272bcf84458367be77c772b8
Serhii Kozlov <sk0621o@gre.ac.uk>
2024-02-01 18:40:05 +0000

    Added basic movement and camera control

--- Coco Game/Assets/Scripts/PlayerMovement.cs
+++ Coco Game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float horizontalMov, verticalMov, speedMov;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMov = Input.GetAxisRaw("Horizontal");
+        verticalMov = Input.GetAxisRaw("Vertical");
+    }
+
+    public void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(horizontalMov, 0, verticalMov);
+        rb.velocity *= speedMov ;
     }
 }
