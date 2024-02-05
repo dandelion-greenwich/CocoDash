d8917add911a4180e0609ec2d932023945836e8c
Shannon Grant <sg1222p@gre.ac.uk>
2024-02-05 20:31:04 +0000

    Modified enemy movement script
    Enemy speed can increase over time

--- Coco Game/Assets/Scripts/EnemyMovement.cs
+++ Coco Game/Assets/Scripts/EnemyMovement.cs
@@ -6,11 +6,8 @@ public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
     public float speed = 5.0f; // Speed at which the enemy moves towards the player
-
-    void Start()
-    {
-        
-    }
+    //public float acceleration = 0.1f; 
+    //public float maxSpeed = 10.0f; // Maximum speed
 
     void Update()
     {
@@ -22,6 +19,9 @@ public class EnemyMovement : MonoBehaviour
 
             // Move the enemy
             transform.position += direction * speed * Time.deltaTime;
+
+            // Increase the speed with time, without exceeding the maximum speed
+           // speed = Mathf.Min(speed + acceleration * Time.deltaTime, maxSpeed);
         }
     }
 }
