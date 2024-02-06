6cc3ecf2230576b2e5b22a1e5447b4fae2bc43a6
Serhii Kozlov <sk0621o@gre.ac.uk>
2024-02-06 15:15:06 +0000

    Improved movement and jumping
    Additionally modified gitignore file and made prefabs of player and cinemachine camera

--- Coco Game/Assets/Scripts/PlayerMovement.cs
+++ Coco Game/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float horizontalMov, verticalMov, speedMov, turnSmoothTime = 0.1f, jumpingHeight;
+    public float horizontalMov, verticalMov, speedMov, turnSmoothTime = 0.1f, jumpingHeight, gravity, yVel;
     private float turnSmoothVelocity;
     public bool jumping;
     Camera mainCamera;
@@ -19,17 +19,24 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMov = Input.GetAxisRaw("Horizontal"); //gets horizontal input for player
         verticalMov = Input.GetAxisRaw("Vertical"); //gets vertical input for player
-        JumpingFunction();
     }
 
     public void FixedUpdate()
     {
+        Movement();
+    }
+
+    public void Movement()
+    {
         Rigidbody rb = GetComponent<Rigidbody>();
+        JumpingFunction(rb);
+        Debug.Log(yVel);
+
         rb.velocity = mainCamera.transform.right * horizontalMov + mainCamera.transform.forward * verticalMov; //makes the player walk towards the camera view
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z).normalized; //calculates the movement
+        rb.velocity = new Vector3(rb.velocity.x, yVel, rb.velocity.z).normalized; //calculates the movement
         rb.velocity *= speedMov * Time.deltaTime; //adjusts the movement by multiplying by speed and deltaTime to make fps the same
 
-        if (rb.velocity.magnitude > 0f) //
+        if (rb.velocity.magnitude > 0f) //changes rotation of the character towards where the camera looks only if the character moves
         {
             float targetAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg; /*+ cam.eulerAngles.y;*/ //calucalets where the character model has to look
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //smoothes the movement
@@ -37,18 +44,30 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void JumpingFunction()
+    public void JumpingFunction(Rigidbody rb)
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
         if (Input.GetKey(KeyCode.Space) && jumping == false)
         {
             jumping = true;
-            rb.AddForce(new Vector3 (0f, jumpingHeight, 0f), ForceMode.Impulse);
+            yVel = jumpingHeight;
+        }
+
+        if (jumping == true)
+        {
+            yVel -= gravity * Time.deltaTime;
+        }
+        else if (jumping == false)
+        {
+            yVel = 0f;
         }
     }
 
     public void OnCollisionEnter(Collision collision)
     {
         jumping = false;
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        jumping = true;
     }
 }
