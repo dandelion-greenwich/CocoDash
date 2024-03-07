e7e44cc5ee72f4c07576b40e7501050e5b84a270
Shannon Grant <sg1222p@gre.ac.uk>
2024-03-07 01:05:49 +0000

    Added Bark to ability script
    needs improvement the enemy stops but does not run away

--- Coco Game/Assets/Scripts/AbilitiesScript.cs
+++ Coco Game/Assets/Scripts/AbilitiesScript.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 
 public class AbilitiesScript : MonoBehaviour
 {
-    public bool dashState, poopState;
+    public bool dashState, poopState, barkState;
     public float dashSpeed;
     public Rigidbody poop;
     [SerializeField] Transform buttPosition;
+    public GameObject barkSpherePrefab;
+    float barkRadius = 5f;
+    float barkDuration = 1f;
+    public AudioClip barkSound;
 
     Rigidbody rb;
+    private AudioSource audioSource;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +28,7 @@ public class AbilitiesScript : MonoBehaviour
     {
         Dash();
         Pooping();
+        Barking();
     }
 
     public void Dash()
@@ -30,7 +38,7 @@ public class AbilitiesScript : MonoBehaviour
             rb.AddForce(transform.forward * dashSpeed + transform.up * dashSpeed / 5, ForceMode.Impulse);
             dashState = false;
         }
-        if(Input.GetKeyUp(KeyCode.Alpha1))
+        if (Input.GetKeyUp(KeyCode.Alpha1))
         {
             dashState = true;
         }
@@ -44,10 +52,31 @@ public class AbilitiesScript : MonoBehaviour
             dropPoop.velocity = new Vector3(0f, -2f, 0f);
             poopState = false;
         }
-        
+
         if (Input.GetKeyUp(KeyCode.Alpha2))
         {
             poopState = true;
+        }
+    }
+
+    public void Barking()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) && barkState)
+        {
+            if (barkSound) audioSource.PlayOneShot(barkSound); // Play bark sound effect
+
+            // Instantiate the bark sphere as a trigger for enemy reaction
+            GameObject barkSphere = Instantiate(barkSpherePrefab, transform.position, Quaternion.identity);
+            barkSphere.transform.localScale = new Vector3(barkRadius, barkRadius, barkRadius);
+            barkSphere.tag = "Barking";
+
+            Destroy(barkSphere, barkDuration); // Ensure the sphere is removed after the effect duration
+            barkState = false; // Prevent spamming the bark ability
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            barkState = true;
         }
     }
 }
