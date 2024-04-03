f131b100f1555f394b0a7125c7166c1f182e9139
Serhii Kozlov <sk0621o@gre.ac.uk>
2024-04-03 15:13:24 +0100

    Made main menu live again after opening it from pause or victory
    Additionally made enemy fov light more intensive. Still main menu animations freezes after game over. Don't know how to fix it, bug doesn't make sense

--- Coco Game/Assets/Scripts/LoadScene.cs
+++ Coco Game/Assets/Scripts/LoadScene.cs
@@ -8,5 +8,6 @@ public class LoadScene : MonoBehaviour
     public void LoadNextScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName); // loads the level when 'start game' is pressed - D'Arcy
+        Time.timeScale = 1f;
     }
 }
